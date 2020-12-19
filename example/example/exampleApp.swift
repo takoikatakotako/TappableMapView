35dbf4041d52d2754ef121304f6e46bf701e8264
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2020-12-19 18:19:16 +0900

    add example projects

---
+++ example/example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by junpei ono on 2020/12/19.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
